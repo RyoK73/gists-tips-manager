db3fa87d43c9ca9177e84b0f237bbf60bbec64b8
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-07-23 16:50:53 +0900

    chore: tagsをcategoryへ名称変更
    - categoryのほうが直感的にわかるため

--- scripts/gists-tips-manager.zsh
+++ scripts/gists-tips-manager.zsh
@@ -15,7 +15,7 @@ function tip-new() {
   local assets_status=(draft uploaded)
   local SCIRPT_DIR="$(cd "$(dirname "${(%):-%x}")" && pwd)"
   local ASSETS_JSON="$(cd "${SCIRPT_DIR}/../assets" && pwd)/assets.json"
-  local assets_tags="$(jq -r '.tags | sort | .[]' "${ASSETS_JSON}")"
+  local assets_category="$(jq -r '.category | sort | .[]' "${ASSETS_JSON}")"
   local assets_lang="$(jq -r '.lang | sort | .[]' "${ASSETS_JSON}")"
 
   print-launch-message "Let's Create Tips !"
@@ -23,7 +23,8 @@ function tip-new() {
   local filename="$(gum input --placeholder=ファイル名を入力してください)
 "
   local title="$(gum input --placeholder=タイトルを入力してください)"
-  local tags="$(gum choose --header=タグを選んでください --no-limit <<<"${assets_tags}")"
+  local category="$(gum choose --header=タグを選んでください --no-limit <<<"${assets_category}")"
+  local category_yaml="[$(echo "${category}" | paste -sd, - | sed 's/,/, /g')]"
   local lang="$(gum choose --header=言語を選んでください --no-limit <<<"${assets_lang}")"
   local created_date="$(date "+%Y-%m-%d")"
   local save_dir="$(cd "${SCIRPT_DIR}/../tips" && pwq)"
@@ -31,16 +32,10 @@ function tip-new() {
   echo \
     "---
     title: ${title}
-    summary: ${summary}
-    tags: ${tags}
-    lang: ${lang}
-    created_at: ${created_time}
-    updated_at:
-    status: draft
-    gist_id: ""
-    gist_url: ""
-    ---" \
-    >>"$fullpath"
+    category: ${category_yaml}
+    created_at: ${created_date}
+    ---
+EOF
 
   if $(gum confirm "${EDITOR}で開きますか？"); then
     $EDITOR "$fullpath"
